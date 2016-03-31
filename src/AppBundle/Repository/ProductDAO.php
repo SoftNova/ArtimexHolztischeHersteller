9971b72d309e91cabce0a7154d58394f719880c9
Cata Visan <Cata Visan>
2016-03-31 21:39:43 +0300

    added project

---
+++ src/AppBundle/Repository/ProductDAO.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rvcat
+ * Date: 3/31/2016
+ * Time: 6:57 PM
+ */
+
+namespace AppBundle\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class ProductDAO extends EntityRepository
+{
+
+}
