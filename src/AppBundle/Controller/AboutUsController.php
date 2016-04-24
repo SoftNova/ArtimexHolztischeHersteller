9bac7ca56d625ee2710355b1e059a20de6886d8b
Bogdan <bogdan.munteanu@soft-nova.com>
2016-04-24 17:00:15 +0300

    about us page

---
+++ src/AppBundle/Controller/AboutUsController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Boogdan
+ * Date: 24.04.2016
+ * Time: 16:11
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class AboutUsController extends Controller
+{
+    /**
+     * @Route("/aboutus", name="aboutus")
+     */
+    public function indexAction(Request $request)
+    {
+
+        // replace this example code with whatever you need
+        return $this->render('client/aboutus.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+}
